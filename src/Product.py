cd2eee2ff008dcf1404fc24770f17ca554b16211
rybin <rybin.32@gmail.com>
2025-08-18 22:54:57 +0300

    hw 14.2 task 3 done

--- src/Product.py
+++ src/Product.py
@@ -13,3 +13,8 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product: dict):
+        new_product = Product(product['name'], product['description'], product['price'], product['quantity'])
+        return new_product
--- src/task_2.py
+++ src/task_2.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     print(category1.products)
     print(category1.product_count)
 
-    # new_product = Product.new_product(
-    #     {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-    #      "quantity": 5})
-    # print(new_product.name)
-    # print(new_product.description)
-    # print(new_product.price)
-    # print(new_product.quantity)
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
     #
     # new_product.price = 800
     # print(new_product.price)
