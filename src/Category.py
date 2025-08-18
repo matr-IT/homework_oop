32c2973adb9ad4c0d4f1ad5d74ee74f83d64b7fd
rybin <rybin.32@gmail.com>
2025-08-18 22:38:26 +0300

    hw 14.2 task 1, task 2 done

--- src/Category.py
+++ src/Category.py
@@ -11,11 +11,22 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        list_of_products = ""
+        for i in self.__products:
+            list_of_products += f'{i.name}, {i.price} руб. Остаток: {i.quantity} шт.\n'
+        return list_of_products
