691769b5767dd39c118870525ed797aa4701ca65
rybin <rybin.32@gmail.com>
2025-08-22 22:15:10 +0300

    hw 15.1 task 1

--- src/Category.py
+++ src/Category.py
@@ -29,7 +29,5 @@ class Category:
 
     @property
     def products(self):
-        list_of_products = ""
         for i in self.__products:
-            list_of_products += f"{i.name}, {i.price} руб. Остаток: {i.quantity} шт.\n"
-        return list_of_products
+            print(i)
--- src/Product.py
+++ src/Product.py
@@ -14,6 +14,9 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
+
     @classmethod
     def new_product(cls, product: dict):
 
